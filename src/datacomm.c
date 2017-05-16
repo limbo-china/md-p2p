760dbd7e8587bd91d2d67b758e5b8eccc137cdd6
limbo <limbo_china@163.com>
2017-05-17 01:58:14 +0800

    update

--- src/datacomm.c
+++ src/datacomm.c
@@ -53,10 +53,10 @@ void initComm(DataComm** comm, struct SpacialStr* space, struct CellStr* cells){
 
             datacomm->neighborProc2[i] = xyz1[0] + globalProcNum[0] *(xyz1[1] + globalProcNum[1]*xyz1[2]);
         }
-        if (getMyRank() == 4)
-            for(int i=0;i<26;i++){
-                printf("%d: %d\n", i, datacomm->neighborProc2[i]);
-            }
+        // if (getMyRank() == 4)
+        //     for(int i=0;i<26;i++){
+        //         printf("%d: %d\n", i, datacomm->neighborProc2[i]);
+        //     }
 
     // 各方向需要通信的细胞数的最大值
     int maxComm = MAX((xyzCellNum[0]+2)*(xyzCellNum[1]+2),
